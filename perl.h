e4744969709b361ec257f0a40948e5fc3105e553
Nicolas R <atoomic@cpan.org>
2020-07-24 19:13:24 -0600

    restore PERL_VERSION_STRING

--- perl.h
+++ perl.h
@@ -3527,7 +3527,7 @@ EXTERN_C int perl_tsa_mutex_unlock(perl_mutex* mutex)
 #endif
 
 #ifndef PERL_FS_VERSION
-#  define PERL_FS_VERSION	PERL_CORE_MINOR_STRING
+#  define PERL_FS_VERSION	PERL_VERSION_STRING
 #endif
 
 /* This defines a way to flush all output buffers.  This may be a
@@ -4659,7 +4659,7 @@ EXTERN_C char **environ;  /* environment variables supplied via exec */
 #include "patchlevel.h"
 #undef PERL_PATCHLEVEL_H_IMPLICIT
 
-#define PERL_CORE_MINOR_STRING	STRINGIFY(PERL_CORE_MAJOR) "." \
+#define PERL_VERSION_STRING	STRINGIFY(PERL_CORE_MAJOR) "." \
 				STRINGIFY(PERL_CORE_MINOR) "." \
 				STRINGIFY(PERL_CORE_RELEASE)
 
