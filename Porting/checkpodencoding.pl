ed009d725da85d1e5235c8f55e4f6c1249c4e7f9
Nicolas R <nicolas@atoomic.org>
2020-08-31 17:26:41 -0500

    Fix multiple warnings for porting/utils.t

--- Porting/checkpodencoding.pl
+++ Porting/checkpodencoding.pl
@@ -61,7 +61,7 @@ sub finder {
 
     if (@non_ascii and not $has_encoding) {
         say "$file:";
-        $DB::single = 1;
+        $DB::single = $DB::single = 1;
         for (@non_ascii) {
             say "    $_->{num} ($_->{encoding}): $_->{line}";
         }
